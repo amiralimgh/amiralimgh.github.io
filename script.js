7eac04ff7135779c6802cab0de515ba21f1f6a16
Amirali Malekghasemi <90844458+amiralimgh@users.noreply.github.com>
2023-01-11 15:50:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const hamMenu = document.querySelector('.hamburger-menu');
+const offscreenMenu = document.querySelector('.offscreen-menu');
+const navbarList = document.querySelector('.navbar-list');
+const navbarLinks = navbarList.querySelectorAll('li');
+
+window.addEventListener('load', () => {
+  //Loading screen
+  var div = $('#overlay');
+  // div.fadeOut(1300);
+  div.fadeOut(0);
+  //Navbar animation
+  navbarList.classList.add('active');
+  document.querySelector('.navbar-list li').classList.add('active');
+});
+
+//Hamburger menu transformation
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offscreenMenu.classList.toggle('active');
+});
+
+
+navbarLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    // Remove the active class from all links
+    navbarLinks.forEach((link) => link.classList.remove('active'));
+    // Add the active class to the clicked link
+    event.target.parentElement.classList.add('active');
+  });
+});
+
+
+// document.querySelectorAll('.navbar-list li a').forEach(link => {
+//   link.addEventListener('click', e => {
+//     const className = e.target.parentElement.className;
+//     const selectedDiv = document.querySelector(`#${className.split('-')[0]}`);
+//     const otherDivs = document.querySelectorAll(`section:not(#${className.split('-')[0]})`);
+//     selectedDiv.style.display = 'block';
+//     otherDivs.forEach(div => div.style.display = 'none');
+//   });
+// });
+
+
+
+$(".navbar-list li").click(function () {
+  // Get the ID of the section to show
+  var arr = $(this).attr("class");
+  //Format the class name
+  var divId = arr.split(" ")[0];
+
+  //Find the current visible section
+  var visibleDiv = $("section:not(:hidden)");
+  //Replace current section with the new one
+  visibleDiv.animate({ height: "0px" }, 500, function () {
+    $(visibleDiv).hide();
+    $("#" + divId).show().animate({ height: "100%" }, 500);
+  });
+});
