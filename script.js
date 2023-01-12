254a66ff9b44e2b7135be56d61b9431a06a292ef
Amirali Malekghasemi <90844458+amiralimgh@users.noreply.github.com>
2023-01-11 20:43:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ window.addEventListener('load', () => {
   //Navbar animation
   navbarList.classList.add('active');
   document.querySelector('.navbar-list li').classList.add('active');
+
+
 });
 
 //Hamburger menu transformation
@@ -55,4 +57,18 @@ $(".navbar-list li").click(function () {
     $(visibleDiv).hide();
     $("#" + divId).show().animate({ height: "100%" }, 500);
   });
+});
+
+
+$.ajax({
+  method: 'POST',
+  url: 'https://formsubmit.co/ajax/your@email.com',
+  dataType: 'json',
+  accepts: 'application/json',
+  data: {
+      name: "FormSubmit",
+      message: "I'm from Devro LABS"
+  },
+  success: (data) => console.log(data),
+  error: (err) => console.log(err)
 });
